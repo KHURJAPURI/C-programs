731c357b86c1179355d3eaf738e84f221878d6e8
KHURJAPURI <181564866+KHURJAPURI@users.noreply.github.com>
2024-10-10 23:55:32 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int area,l,b;
+	printf("enter the length of rectangle and bearth of rectangle");
+	scanf("%d %d",&l,&b);
+	area=l*b;
+	printf("area of rectangle is:- %d \n",area);
+	return 0;
+}
+
